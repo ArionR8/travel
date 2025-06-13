38ff0c70b6074a46158229083c964656ab696fca
ArionR8 <149816094+ArionR8@users.noreply.github.com>
2025-06-13 17:17:06 +0200

    Revert "added .env"
    This reverts commit d79a1c4877df498ef3e6adce4fb4e71fd7355e22.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,18 @@
 // /src/services/api.ts
-import { API_BASE_URL } from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { API_BASE_URL } from "../config/config";
+
 const api = axios.create({
-  baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem("auth_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = await AsyncStorage.getItem("auth_token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
